e734ef8ffa208ac51883f5d47a90681911e06a69
abhayaaatriv <abhaya002btaiml23@igdtuw.ac.in>
2025-04-03 19:28:57 +0530

    added home by harman

--- app.py
+++ app.py
@@ -94,6 +94,10 @@ def send_certificate_email(recipient_email, name, cert_path):
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
